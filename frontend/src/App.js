d4d15ca0ed49b3117a150b981ff91cfc201b95dc
Lorenzo Windmoller <lorenzomart01@gmail.com>
2019-10-08 13:44:42 -0400

    Atualizado o corpo do react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1>Teste</h1>
     </div>
   );
 }
