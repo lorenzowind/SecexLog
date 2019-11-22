05c07cfad4e33b3510c2b959086c25542697088d
Leonardo <sonleo2013@gmail.com>
2019-11-22 11:32:01 -0400

    autenticação modificada novamente

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
