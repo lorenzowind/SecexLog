a67ba54c527e37dbbd9bba663a51c0ecb03381bf
Gustavo Fadel <gustavohenriquefrancofadel@gmail.com>
2020-02-07 17:40:37 -0400

    Initial TelaDetalhes commit

---
+++ frontend/src/pages/ConsultaManual/TelaDetalhes/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+export default class TelaDetalhes extends Component {
+  render () {
+    return (
+      <div></div>
+    );
+  }
+}
