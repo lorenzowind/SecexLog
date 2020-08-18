1306aa696dac062f70ce004debbd3ad229f9d407
lorenzowind <lorenzomart01@gmail.com>
2020-08-18 16:53:00 -0400

    chore: docker pre-configuration

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+import routes from './routes';
+import AppError from '../../errors/AppError';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333!');
+});
