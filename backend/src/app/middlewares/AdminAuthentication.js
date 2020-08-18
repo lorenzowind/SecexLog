67c85551ce416aa6ebbfabe94b26ee15357ccdae
lorenzowind <lorenzomart01@gmail.com>
2020-08-18 15:01:35 -0400

    chore: initial backend structure

--- backend/src/app/middlewares/AdminAuthentication.js
+++
@@ -1,9 +0,0 @@
-module.exports = middleware => {
-  return (req, res, next) => {
-    if (req.user.cargo === "Administrador") {
-      middleware(req, res, next);
-    } else {
-      res.status(401).send("Acesso somente para administradores");
-    }
-  };
-};
